8fe0ed17f27b86eb41f544c79efe6369f2d5a69a
Doakes <your.email@example.com>
2026-05-30 11:29:17 +0530

    Optimize Docker image with production requirements

--- backend/db/db_config.py
+++ backend/db/db_config.py
@@ -2,7 +2,7 @@ import psycopg2
 
 def get_connection():
     return psycopg2.connect(
-        host="localhost",
+        host="host.docker.internal",
         dbname="fraudx",
         user="postgres",
         password="postgres123"  # <-- Must match what you just set
